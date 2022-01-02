d1d2486498f00c6f1c04e137c6f2854658e24c61
Loa212 <anobileloris@gmail.com>
2022-01-02 23:58:50 +0100

    heroku & db settings

--- config/database.js
+++ config/database.js
@@ -1,11 +1,41 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+
+  if (env("NODE_ENV") === "production") {
+    const config = parse(process.env.DATABASE_URL);
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+          },
+          options: {
+            ssl: false, //play around when deployed
+          },
+        },
+      },
+    };
+  }
+
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: "sqlite",
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db")
+        ),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
