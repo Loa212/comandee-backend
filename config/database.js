5478b377bcac198ff011f9e17e156631e8ff9c52
Loa212 <anobileloris@gmail.com>
2022-01-03 22:47:21 +0100

    changes for tutorial

--- config/database.js
+++ config/database.js
@@ -6,25 +6,19 @@ module.exports = ({ env }) => {
   if (env("NODE_ENV") === "production") {
     const config = parse(process.env.DATABASE_URL);
     return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "bookshelf",
-          settings: {
-            client: 'postgres',
-            host: env('DATABASE_HOST', '127.0.0.1'),
-            port: env.int('DATABASE_PORT', 5432),
-            database: env('DATABASE_NAME', 'strapi'),
-            username: env('DATABASE_USERNAME', 'strapi'),
-            password: env('DATABASE_PASSWORD', 'strapi'), 
-            ssl: {
-              rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-            },     
-          },
-          options: {
-            ssl: env.bool('DATABASE_SSL', false),
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false
           },
         },
+        debug: false,
       },
     };
   }
