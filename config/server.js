5208c1ac6783929658b0bd0b845a65ea97b32dfb
Loa212 <anobileloris@gmail.com>
2022-01-03 18:28:08 +0100

    changes to server & database

--- config/server.js
+++ config/server.js
@@ -1,4 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5965d14c-2739-4274-bdeb-cd7608b81fbd'),
+    },
+  },
 });
