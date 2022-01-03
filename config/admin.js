0aa7cccd8330654c07a2c99d0365821987b844ba
Loa212 <anobileloris@gmail.com>
2022-01-03 22:30:40 +0100

    admin & database changes

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', '1vl5j0anles8ejmf4qf69h99r3zazw0ydmpsy4p'),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', '1a2cc2628af1bb6f61316af2a5f7bd1f'),
   },
--- config/database.js
+++ config/database.js
@@ -15,8 +15,8 @@ module.exports = ({ env }) => {
             host: env('DATABASE_HOST', '127.0.0.1'),
             port: env.int('DATABASE_PORT', 5432),
             database: env('DATABASE_NAME', 'strapi'),
-            username: env('DATABASE_USERNAME', ''),
-            password: env('DATABASE_PASSWORD', ''), 
+            username: env('DATABASE_USERNAME', 'strapi'),
+            password: env('DATABASE_PASSWORD', 'strapi'), 
             ssl: {
               rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
             },     
