bf3d12e9e369f84989e43514c8a5a009322f028f
Loa212 <anobileloris@gmail.com>
2022-01-03 18:37:05 +0100

    changes on db

--- config/database.js
+++ config/database.js
@@ -11,12 +11,12 @@ module.exports = ({ env }) => {
         default: {
           connector: "bookshelf",
           settings: {
-            client: "postgres",
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
+            client: 'postgres',
+            host: env('DATABASE_HOST', '127.0.0.1'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', 'strapi'),
+            username: env('DATABASE_USERNAME', ''),
+            password: env('DATABASE_PASSWORD', ''), 
             ssl: {
               rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
             },     
