8d8d7bb95803e729a6e1bd3fb472389523b4c1ec
Loa212 <anobileloris@gmail.com>
2022-01-04 18:48:02 +0100

    update aws provider

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,4 +1,24 @@
-module.exports = [
+module.exports = ({ env }) => [
+
+  //updates from
+  //https://github.com/zoomoid/strapi-provider-upload-aws-s3-advanced
+  //needed to allow the thumbnails to properly populate
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", `${env("CDN_BASE_URL")}`],
+          "media-src": ["'self'", "data:", "blob:", `${env("CDN_BASE_URL")}`],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  //end updates
+
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
