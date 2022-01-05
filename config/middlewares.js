5179600a5aa4d214c2e0c18786634254f9897238
Loa212 <anobileloris@gmail.com>
2022-01-05 23:26:52 +0100

    order matters in middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,6 +3,7 @@ module.exports = ({ env }) => [
   //updates from
   //https://github.com/zoomoid/strapi-provider-upload-aws-s3-advanced
   //needed to allow the thumbnails to properly populate
+  'strapi::errors',
   {
     name: "strapi::security",
     config: {
@@ -10,8 +11,10 @@ module.exports = ({ env }) => [
         useDefaults: true,
         directives: {
           "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "sheeesh", `//${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`, `${env("CDN_BASE_URL")}`],
-          "media-src": ["'self'", "data:", "blob:", `//${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`, `${env("CDN_BASE_URL")}`],
+          "img-src": ["'self'", "data:", "blob:", `${env("AWS_BASE_URL")}`],
+          "media-src": ["'self'", "data:", "blob:", `${env("AWS_BASE_URL")}`],
+          // "img-src": ["'self'", "data:", "blob:", `//${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`],
+          // "media-src": ["'self'", "data:", "blob:", `//${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`],
           upgradeInsecureRequests: null,
         },
       },
@@ -19,8 +22,7 @@ module.exports = ({ env }) => [
   },
   //end updates
 
-  'strapi::errors',
-  'strapi::security',
+  //'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
